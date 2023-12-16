8d2cbce0ad0bd75b23049fd1e8e800e56ffc0c6f
Jobayer <dev.yubayer@gmail.com>
2023-12-16 08:53:27 +0600

    Kyon147 package setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+use Osiset\ShopifyApp\Contracts\ShopModel as IShopModel;
+use Osiset\ShopifyApp\Traits\ShopModel;
+
+class User extends Authenticatable implements IShopModel
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use ShopModel;
 
     /**
      * The attributes that are mass assignable.
@@ -40,6 +44,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        // 'password' => 'hashed',
     ];
 }
